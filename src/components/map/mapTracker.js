2d5d781bb88f1f44665b3ff16ad9b85cac8a5430
Rafał Zakrzewski <eltharis.dev@gmail.com>
2023-08-09 20:18:52 +0200

    adding-marker

--- src/components/map/mapTracker.js
+++ src/components/map/mapTracker.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View } from "react-native";
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import mapStyles from "./styles";
 
 const MapTracker = () => {
@@ -14,7 +14,16 @@ const MapTracker = () => {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-            />
+            >
+                <Marker
+                    coordinate={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                    }}
+                    image={require('../../assets/space-station.png')}
+                />
+            </MapView>
+
         </View>
     )
 }
